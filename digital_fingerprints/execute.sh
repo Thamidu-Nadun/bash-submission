177123e8b6987d51ae27f2e31f080e580d9d6fd1
Thamidu-Nadun <nadunrz101@gmail.com>
2025-11-23 15:44:19 +0530

    1

--- digital_fingerprints/execute.sh
+++ digital_fingerprints/execute.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ROOT_PATH="./src"
 OUTPUT_PATH="./out/checksums.txt"
-mkdir -p ./out
+
+mkdir -p ./out;
+echo "" > $OUTPUT_PATH
 
 for file in $(find $ROOT_PATH -type f); do
     checksum=$(md5sum "$file" | awk '{ print $1 }')
-    # echo "$checksum $file" >> $OUTPUT_PATH
+    echo "$checksum $file" >> $OUTPUT_PATH
 done
