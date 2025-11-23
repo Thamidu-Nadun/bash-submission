8d0783d5fb6a888f72ba07fec2c7e0263e6eb438
Thamidu-Nadun <nadunrz101@gmail.com>
2025-11-23 15:19:00 +0530

    1

--- the_lost/execute.sh
+++ the_lost/execute.sh
@@ -5,20 +5,17 @@ install_xml2json(){
     if ! command -v xml2json &> /dev/null; then
         echo "xml2json could not be found, installing..."
         npm i -g xml2json-cli
-    else
-        echo "xml2json is already installed."
+        export PATH="$(npm bin -g):$PATH"
     fi
 }
 
-if ! command -v xml2json &> /dev/null; then
-    echo "xml2json is not installed. Attempting to install..."
-    install_xml2json
-    exit 1
-fi
+install_xml2json
 
 mkdir -p ./out
 XML_FILE="./src/data.xml"
 JSON_FILE="./out/output.json"
 
 xml2json $XML_FILE $JSON_FILE
+
 echo "Converted $XML_FILE → $JSON_FILE"
+
