79a28bb9bb212890a054664f59453dd41ead59dc
Thamidu-Nadun <nadunrz101@gmail.com>
2025-11-23 14:32:24 +0530

    1

--- echo/execute.sh
+++ echo/execute.sh
@@ -5,8 +5,8 @@ F="./src/echoes/pattern.csv"
 awk -F',' '
 NR>1 {dur[NR-2] = $2}    # store first four durations in array
 END {
-    # 7-beat palindrome: A, B, C, D, C, B, A
+    # A, B, C, D, C, B, A
     sum = dur[0] + dur[1] + dur[2] + dur[3] + dur[2] + dur[1] + dur[0]
-    printf "%.3f\n", sum/1000   # convert to seconds with 3 decimal places
+    printf "PALINDROME_DURATION:%.3f\n", sum/1000
 }
 ' $F
