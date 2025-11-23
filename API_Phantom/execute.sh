c63a02cdca535e18ca6bb23fcb454ed1baac3b5d
Thamidu-Nadun <nadunrz101@gmail.com>
2025-11-23 10:20:22 +0530

    1

--- API_Phantom/execute.sh
+++ API_Phantom/execute.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+URL="https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+RES=$(curl -s "$URL")
+
+echo "$RES" > price
+price=$(echo "$RES" | cut -d':' -f3 | cut -d'}' -f1)
+
+echo "$price"
+rm price
