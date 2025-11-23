06747cc431fbec1a436b6a6d87295435b5ecedd7
Thamidu-Nadun <nadunrz101@gmail.com>
2025-11-23 11:04:10 +0530

    1

--- Archive_Archaeologist/execute.sh
+++ Archive_Archaeologist/execute.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 file="./src/archive.tar.gz"
-gunzip -c "$file" | tar -xvf -
-gunzip -c "level2.tar.gz" | tar -xvf -
-rm level2.tar.gz
-gunzip -c "level3.tar.gz" | tar -xvf -
-rm level3.tar.gz
-cat treasure.txt
-rm treasure.txt
+
+gunzip -c "$file" | tar -xvf - -C ./src
+gunzip -c "./src/level2.tar.gz" | tar -xvf - -C ./src
+gunzip -c "./src/level3.tar.gz" | tar -xvf - -C ./src
+
+cat ./src/treasure.txt
